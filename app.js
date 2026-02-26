4708791abc32f13435debb14ff0fe4d31e4dd7db
khahcmute <trantuankhapc@gmail.com>
2026-02-26 11:53:36 +0700

    add todos array and render tasks to DOM

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const todoInput = document.querySelector("#todo-input");
 const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
+
+let todos = [];
 
 todoForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(todoInput.value);
+
+  let todoText = todoInput.value.trim();
+  if (todoText == "") return;
+  todos.push(todoText);
+  renderTodos();
+  todoInput.value = "";
 });
+
+function renderTodos() {
+  todoList.innerHTML = "";
+  todos.forEach(function (todo) {
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    let span = document.createElement("span");
+    span.textContent = todo;
+    let deleteBtn = document.createElement("i");
+    deleteBtn.textContent = "x";
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    todoList.appendChild(li);
+  });
+}
